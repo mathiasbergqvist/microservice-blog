20447d2bcf5f7f2aa3f59a557ac368c31dffb689
Mathias <mathiasbergqvist88@gmail.com>
2021-01-06 14:36:16 +0100

    GET/POST endpoint for posts

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+// Enable usage of body parser
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  // Constructing a random byte id
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("🚀 POSTS SERVICE - Listening on 4000");
+});
