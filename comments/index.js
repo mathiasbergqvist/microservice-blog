8beb6f14b54c88a39cc626e6c3f48c98ead0b232
Mathias <mathiasbergqvist88@gmail.com>
2021-01-07 19:03:48 +0100

    Emmitting comment creation events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 // Enable usage of body parser
@@ -15,16 +16,27 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
+  const postId = req.params.id;
 
   // Comments lookup
-  const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[postId] || [];
 
   // Add comment to index
   comments.push({ id: commentId, content });
-  commentsByPostId[req.params.id] = comments;
+  commentsByPostId[postId] = comments;
+
+  //Emit event
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  });
 
   res.status(201).send(comments);
 });
