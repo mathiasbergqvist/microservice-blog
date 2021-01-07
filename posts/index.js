3cad4608c707f3e70962c9072969527c576e8107
Mathias <mathiasbergqvist88@gmail.com>
2021-01-07 19:17:39 +0100

    Receiving events

--- posts/index.js
+++ posts/index.js
@@ -23,6 +23,7 @@ app.post("/posts", async (req, res) => {
 
   posts[id] = { id, title };
 
+  //Emit event
   await axios.post("http://localhost:4005/events", {
     type: "PostCreated",
     data: {
@@ -32,6 +33,11 @@ app.post("/posts", async (req, res) => {
   });
 
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
