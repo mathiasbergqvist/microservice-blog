a029396fdbae5c97338a5987a338759209408fbc
Mathias <mathiasbergqvist88@gmail.com>
2021-01-08 11:28:23 +0100

    Rendering comments by status

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((comment) => (
-    <li key={comment.id}>{comment.content}</li>
-  ));
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    switch (comment.status) {
+      case "approved":
+        content = comment.content;
+        break;
+      case "pending":
+        content = "This comments is awaiting moderation";
+        break;
+      case "rejected":
+        content = "This comment has been rejected";
+        break;
+      default:
+        content = "Cannot display comment";
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
 
   return <ul>{renderedComments}</ul>;
 };
