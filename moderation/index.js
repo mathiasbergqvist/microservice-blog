7ef1bb5edc870b61278614709f0b9fbfbfb6b0e7
Mathias <mathiasbergqvist88@gmail.com>
2021-01-08 08:50:56 +0100

    Handling moderation

--- moderation/index.js
+++ moderation/index.js
@@ -6,7 +6,29 @@ const app = express();
 // Enable usage of body parser
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const ILLEGAL_WORD = "orange";
+    const status = data.content.includes(ILLEGAL_WORD)
+      ? "rejected"
+      : "approved";
+
+    // Emit event
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+
+    res.send({});
+  }
+});
 
 app.listen(4003, () => {
   console.log("🚀 MODERATION SERVICE - Listening on 4003");
